5cca3309d4e69a0b659969bdd654274e4f4acd00
Jordan Deveau <jordandeveau@Jordans-iMac.local>
2020-11-20 11:04:50 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const data = require('./data')
+
+const PORT = process.env.PORT
+
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.get('/api/products', (req, res) => {
+  res.status(200).send(data)
+})
+
+// Runs server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
